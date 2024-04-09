683cf035987865157b93e24d735dacf99084347b
OpenHe1mer42 <138040685+OpenHe1mer42@users.noreply.github.com>
2024-04-09 18:03:21 +0200

    Add files via upload

---
+++ Pikado/index.js
@@ -0,0 +1,8 @@
+let scorebutton = document.getElementById('button1');
+let score1 = 0;
+console.log(score1);
+scorebutton.addEventListener('click', function(event) {
+       let score1 = 101;
+
+      
+    }); 
